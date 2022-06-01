9e066c8930ea3c612a5ba89ba561f3f0696d6dbf
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-06-01 14:45:44 +0700

    Initial Commit

---
+++ mininote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mininote
+//
+//  Created by Truong Nhat Anh on 01/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mininote/mininoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  mininoteApp.swift
+//  mininote
+//
+//  Created by Truong Nhat Anh on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct mininoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
